996cd304456de162c46a3a6f085a3f362e642f5d
lekha888 <firstnestai@gmail.com>
2026-07-31 18:59:11 +0530

    feat: replace DM_Sans with Cormorant_Garamond font in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next'
-import { Inter, Fraunces, DM_Sans } from 'next/font/google'
+import { Inter, Cormorant_Garamond } from 'next/font/google'
 import './globals.css'
 import { BottomNav } from '@/components/navigation/BottomNav'
 import { Footer } from '@/components/layout/Footer'
